71449a5cc1a39db01f9cc6ce7f4347655bc63622
Surrui <Surrui@HAL9000>
2015-10-28 16:13:01 +0100

    Commit: Error al crear Participants

--- JocCorda/src/net/jimenez/corda/App.java
+++ JocCorda/src/net/jimenez/corda/App.java
@@ -1,7 +1,5 @@
 package net.jimenez.corda;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import acm.graphics.GImage;
@@ -14,68 +12,59 @@ public class App extends GraphicsProgram {
 	private static final int MIN_FOR = 5;
 	private static final int MAX_FOR = 10;
 	
+	private static Random RND = new Random();
+
 	private static final long serialVersionUID = 4179378993475675411L;
 
 	public void run() {
-		
-		Random rnd = new Random();
 
+		Joc j = new Joc();
 		Corda c = new Corda(490, 350, 300, 10);
 		add(c.crearCorda());
-		
-		List<Participant> EquipA = new ArrayList<Participant>();
-		List<Participant> EquipB = new ArrayList<Participant>();
-		
-		int persones = totalParticipants(rnd);
-		
+
+		int persones = totalParticipants();
+
+		System.out.println("Total participants " + persones);
+
+		GImage img = new GImage("pawn-147060_1280.png");
+		img.scale(0.05);
+		add(img);
+
 		for (int i = 0; i < persones; i++) {
 
-			int maxFor = rnd.nextInt((MAX_FOR - MIN_FOR) + 1) + MIN_FOR;
+			System.out.println("Entra");
 			
-			Participant P = generarParticipant(maxFor);
-			
-			if (i < persones / 2) {
-				
-				EquipA.add(P);
-				
-			} else {
-				
-				EquipB.add(P);
-				
-			}
+			j.afegirPersona(crearParticipant(img));
+
 		}
-		
 	}
-	
+
 	public void init() {
-		
+
 		setSize(1280, 720);
-		
+
 	}
-	
-	private int totalParticipants(Random rnd) {
-		
-		int totalPersones = rnd.nextInt((MAX_PER - MIN_PER) + 1) + MIN_PER;
+
+	private Participant crearParticipant(GImage img) {
+
+		int maxFor = RND.nextInt((MAX_FOR - MIN_FOR) + 1) + MIN_FOR;
+
+		Participant P = new Participant(maxFor, img);
+
+		return P;
+	}
+
+	private int totalParticipants() {
+
+		int totalPersones = RND.nextInt((MAX_PER - MIN_PER) + 1) + MIN_PER;
 
 		if (totalPersones % 2 != 0) {
 
 			totalPersones = totalPersones - 1;
 
 		}
-		
-		return totalPersones;
-		
-	}
-	
-	private Participant generarParticipant(int f) {
-		
-		GImage img = new GImage("pawn-147060_1280.png");
-		img.scale(0.05);
-		add(img);
-		Participant P = new Participant (f, img);
-	    
-	    return P;
-		
-	}
 
+		return totalPersones;
+
+	}
 }
--- JocCorda/src/net/jimenez/corda/Joc.java
+++ JocCorda/src/net/jimenez/corda/Joc.java
@@ -1,12 +1,22 @@
 package net.jimenez.corda;
 
+import java.util.List;
+
 public class Joc {
-	
+
+	List<Participant> Participants;
+	List<Participant> EquipA;
+	List<Participant> EquipB;
+
 	public Joc() {
-		
+
+	}
+
+	public void afegirPersona(Participant p) {
+
+		Participants.add(p);
+
 	}
 	
-	public void iniciar () {	
-		
-	}
+	
 }
